d3b2717edda2b7a829a8ec395b6629facdf4f351
kenny <krogers@hey.com>
2022-10-01 23:35:22 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Gun from "gun";
+
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState();
+  const [postList, setPostList] = useState([]);
+  const [postContent, setPostContent] = useState("");
+
+  let gun = Gun();
+
+  useEffect(() => {
+    setPosts(gun.get("posts"));
+  }, []);
+
+  useEffect(() => {
+    if (posts !== undefined) {
+      const postArray = [];
+      posts.map().on((post, id) => {
+        postArray.push(post);
+      });
+      setPostList(postArray);
+    }
+  }, [posts]);
+
+  const handlePostContentChange = (event) => {
+    event.persist();
+    setPostContent(event.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    posts.set(postContent);
+    setPostList([...postList, postContent]);
+    setPostContent("");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            name="postContent"
+            value={postContent}
+            onChange={handlePostContentChange}
+          />
+          <button type="submit">Add Post</button>
+        </form>
+        {postList !== undefined
+          ? postList.map((post, index) => {
+              return <p key={index}>{post}</p>;
+            })
+          : ""}
       </header>
     </div>
   );
